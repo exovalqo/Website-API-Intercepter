d415c5a63d4b590797a1e87e72b5df6a6caee0b0
Matthew Lue <pudy13531@gmail.com>
2020-10-18 17:26:28 -0500

    Adding Initial Files

---
+++ FiddlerHelper/Form1.Designer.cs
@@ -0,0 +1,354 @@
+﻿namespace FiddlerHelper
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.checkBox4 = new System.Windows.Forms.CheckBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(179, 18);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Exovalqo\'s API Hacker";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(50, 48);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(619, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 51);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(32, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "URL:";
+            this.label2.Click += new System.EventHandler(this.Label2_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(194, 146);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(187, 113);
+            this.textBox2.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(197, 130);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(85, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Response Body:";
+            // 
+            // button1
+            // 
+            this.button1.ForeColor = System.Drawing.Color.DarkGreen;
+            this.button1.Location = new System.Drawing.Point(675, 46);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Watch";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 307);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(162, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Console Output (Error Checking):";
+            // 
+            // listView1
+            // 
+            this.listView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2});
+            this.listView1.GridLines = true;
+            this.listView1.Location = new System.Drawing.Point(12, 323);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(738, 179);
+            this.listView1.TabIndex = 9;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            this.listView1.SelectedIndexChanged += new System.EventHandler(this.ListView1_SelectedIndexChanged);
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.DisplayIndex = 1;
+            this.columnHeader1.Text = "Information";
+            this.columnHeader1.Width = 694;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.DisplayIndex = 0;
+            this.columnHeader2.Text = "#";
+            this.columnHeader2.Width = 40;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(675, 294);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 10;
+            this.button2.Text = "Clear";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox3.Location = new System.Drawing.Point(396, 146);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.ReadOnly = true;
+            this.textBox3.Size = new System.Drawing.Size(357, 113);
+            this.textBox3.TabIndex = 11;
+            // 
+            // label5
+            // 
+            this.label5.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label5.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.label5.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label5.Location = new System.Drawing.Point(1, 277);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(764, 5);
+            this.label5.TabIndex = 12;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(393, 130);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(132, 13);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "Output For Specified URL:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.ForeColor = System.Drawing.Color.Maroon;
+            this.label7.Location = new System.Drawing.Point(12, 71);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(513, 13);
+            this.label7.TabIndex = 14;
+            this.label7.Text = "Press \"Yes\" On Certificate Pop-up Before Application Startup To Prevent Authentic" +
+    "ation and Security Errors";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.ForeColor = System.Drawing.Color.DarkGreen;
+            this.label8.Location = new System.Drawing.Point(197, 23);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(93, 13);
+            this.label8.TabIndex = 15;
+            this.label8.Text = "Experimental Build";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(675, 117);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 16;
+            this.button3.Text = "Clear";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.Button3_Click);
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(6, 146);
+            this.textBox4.Multiline = true;
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(176, 113);
+            this.textBox4.TabIndex = 17;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(9, 130);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(77, 13);
+            this.label9.TabIndex = 18;
+            this.label9.Text = "Request Body:";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(12, 87);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(157, 17);
+            this.checkBox1.TabIndex = 19;
+            this.checkBox1.Text = "Change Request Body Only";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.CheckBox1_CheckedChanged);
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(194, 87);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(165, 17);
+            this.checkBox2.TabIndex = 20;
+            this.checkBox2.Text = "Change Response Body Only";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            this.checkBox2.CheckedChanged += new System.EventHandler(this.CheckBox2_CheckedChanged);
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Location = new System.Drawing.Point(194, 110);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(170, 17);
+            this.checkBox3.TabIndex = 21;
+            this.checkBox3.Text = "Completely New; Custom Body";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            this.checkBox3.CheckedChanged += new System.EventHandler(this.CheckBox3_CheckedChanged);
+            // 
+            // checkBox4
+            // 
+            this.checkBox4.AutoSize = true;
+            this.checkBox4.Location = new System.Drawing.Point(12, 110);
+            this.checkBox4.Name = "checkBox4";
+            this.checkBox4.Size = new System.Drawing.Size(170, 17);
+            this.checkBox4.TabIndex = 22;
+            this.checkBox4.Text = "Completely New; Custom Body";
+            this.checkBox4.UseVisualStyleBackColor = true;
+            this.checkBox4.CheckedChanged += new System.EventHandler(this.CheckBox4_CheckedChanged);
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.ForeColor = System.Drawing.Color.Gray;
+            this.label10.Location = new System.Drawing.Point(626, 9);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(124, 13);
+            this.label10.TabIndex = 23;
+            this.label10.Text = "Discord: Exovalqo#0931";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(762, 514);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.checkBox4);
+            this.Controls.Add(this.checkBox3);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.CheckBox checkBox4;
+        private System.Windows.Forms.Label label10;
+    }
+}
+
---
+++ FiddlerHelper/Form1.cs
@@ -0,0 +1,269 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Fiddler;
+using Telerik.NetworkConnections;
+using System.Collections;
+
+namespace FiddlerHelper
+{
+    public partial class Form1 : Form
+    {
+
+        bool hasCert;
+        int numLines = 0;
+        ArrayList arrUrls;
+        ArrayList responseBodies;
+        public Form1()
+        {
+            InitializeComponent();
+            hasCert = InstallCert();
+            TextBox.CheckForIllegalCrossThreadCalls = false;
+            ListView.CheckForIllegalCrossThreadCalls = false;
+            FiddlerApplication.BeforeResponse += BeforeResponse;
+            FiddlerApplication.Log.OnLogString += LogString;
+            FiddlerApplication.ResponseHeadersAvailable += ResponseHeaderAvaliable;
+            FiddlerApplication.BeforeRequest += BeforeRequest;
+            listView1.Columns[0].Width = -2; //Information Column (I know, out of order) 
+            listView1.Columns[1].Width = -2; // # Column 
+        }
+
+        private void ResponseHeaderAvaliable(Session oSession)
+        {
+            if (oSession.fullUrl.ToLower().Equals(textBox1.Text.ToLower()))
+            {
+
+                bool isText = oSession.oResponse.MIMEType.Contains("text/html");
+                if (isText)
+                {
+                    oSession.bBufferResponse = true;
+                    listView1.Items.Add("Added bBufferResponse!");
+                }
+            }
+           
+
+        }
+        private void Label2_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void autoScroll()
+        {
+            int index = listView1.Items.Count - 1;
+            try
+            {
+                listView1.Items[index].Focused = true;
+            }catch(NullReferenceException ex)
+            {
+
+
+            }
+            listView1.Items[index].Selected = true;
+            listView1.EnsureVisible(index);
+
+        }
+
+        private void BeforeRequest(Session oSession)
+        {
+            for (int i = 0; i < arrUrls.Count; i++)
+            {
+                string temp = (string)arrUrls[i];
+                if (oSession.uriContains(temp))
+                {
+                    if (checkBox1.Checked) // Checks If "Request Body Change Only" Checked
+                    {
+                        oSession.utilDecodeRequest();
+                        oSession.utilSetRequestBody(textBox4.Text);
+                    }
+                    if(checkBox3.Checked)// Response Completely Change
+                    {
+                        // oSession.utilDecodeResponse();
+                        oSession.utilCreateResponseAndBypassServer();
+                    }
+                }
+            }
+        }
+        private void BeforeResponse(Session oSession)
+        {
+            for (int i = 0; i < arrUrls.Count; i++)
+            {
+                string temp = (string)arrUrls[i];
+                if (oSession.uriContains(temp))
+                {
+                    if (checkBox2.Checked || checkBox3.Checked)
+                    {
+                        // oSession.utilDecodeResponse();
+
+                        if (responseBodies.Count == 1)
+                            oSession.utilSetResponseBody((string)responseBodies[0]);
+                        else
+                            oSession.utilSetResponseBody((string)responseBodies[i]);
+                        oSession.utilDecodeResponse();
+                        string oBody1 = oSession.GetResponseBodyAsString();
+                        listView1.Items.Add(new ListViewItem(new[] { "Response Changed At " + oSession.fullUrl + " to \"" + oBody1 + "\"", (++numLines).ToString() }));
+                        textBox3.AppendText(String.Format("\r\nURL: " + temp + " | Response Body Changed To: [{0}]", oBody1));
+
+                        autoScroll();
+                    }
+                }
+            }
+            
+           
+        }
+        private void LogString(object sender, LogEventArgs e)
+         {
+
+            listView1.Items.Add(new ListViewItem(new string[] { "Log " + e.LogString, "" + ++numLines }));
+            autoScroll();
+        }
+        private void activateArrayLists()
+        {
+            string temp = textBox1.Text;
+            if (temp.Contains(";"))
+            {
+                while (temp.Contains(";"))
+                {
+                    string substring = temp.Substring(0, temp.IndexOf(";"));
+                    arrUrls.Add(substring);
+                    temp = temp.Remove(0, temp.IndexOf(";") + 1);
+
+                }
+                if (!String.IsNullOrEmpty(temp))
+                    arrUrls.Add(temp);
+            }
+            else
+                arrUrls.Add(temp);
+
+            string temp2 = textBox2.Text;
+            if (temp2.Contains("\\;"))
+            {
+                while (temp2.Contains("\\;"))
+                {
+                    string substring = temp2.Substring(0, temp2.IndexOf("\\;"));
+                    responseBodies.Add(substring);
+                    temp2 = temp2.Remove(0, temp2.IndexOf("\\;") + 2);
+
+                }
+                if (!String.IsNullOrEmpty(temp2))
+                    responseBodies.Add(temp2);
+            }
+            else
+                responseBodies.Add(temp2);
+
+        }
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            label7.Visible = false;
+            
+            arrUrls = new ArrayList();
+            responseBodies = new ArrayList();
+            activateArrayLists();
+
+
+            if (button1.Text.Equals("Watch"))
+            {
+                button1.Text = "Stop";
+                button1.ForeColor = Color.IndianRed;
+                if (CertMaker.rootCertExists())
+                {
+                    if (!FiddlerApplication.IsStarted())
+                    {
+
+
+                        listView1.Items.Add(new ListViewItem(new string[] { "[!] Success: Found Certificate", ++numLines + "" }));
+                        int port = 5555;
+                      
+
+                        FiddlerApplication.Startup(port, FiddlerCoreStartupFlags.Default);
+                        listView1.Items.Add(new ListViewItem(new string[] { "Created endpoint listening on port " + port,++numLines + "" }));
+                        listView1.Items.Add(new ListViewItem(new string[] { "Starting with settings: Default", ++numLines + "" }));
+                        listView1.Items.Add(new ListViewItem(new string[] { "GateWay: " + CONFIG.UpstreamGateway, ++numLines + "" }));
+
+
+                        textBox3.Text = "Starting... \r\nStarting Proxy On Port: " + port + ". With Proxy Type: " + CONFIG.UpstreamGateway.ToString();
+                        textBox3.AppendText("\r\nUrls being watched: ");
+                        for(int i = 0; i < arrUrls.Count; i++)
+                        {
+                            textBox3.AppendText("\r\n" + arrUrls[i]);
+                        }
+
+                    }
+                    else
+                        listView1.Items.Add("Fiddler Already Started");
+                    
+                }
+                else
+                {
+                    listView1.Items.Add(++numLines + ":[?] Error: Couldn't Find Certificate");
+                    // Add Create certificate button
+                }
+            }else
+            {
+                button1.Text = "Watch";
+                button1.ForeColor = Color.DarkGreen;
+                FiddlerApplication.oProxy.Detach();
+                FiddlerApplication.Shutdown();
+            }
+        }
+       
+        private bool InstallCert()
+        {
+            if(!CertMaker.rootCertExists())
+            {
+                if (!CertMaker.createRootCert())
+                    return false;
+
+                if (!CertMaker.trustRootCert())
+                    return false;
+
+            }
+            return true;
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            listView1.Items.Clear();
+            numLines = 0;
+        }
+
+        private void ListView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button3_Click(object sender, EventArgs e)
+        {
+            textBox3.Text = "Cleared";
+        }
+
+        private void CheckBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if(checkBox1.Checked)
+             checkBox4.Checked = false;
+        }
+
+        private void CheckBox4_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox4.Checked)
+                checkBox1.Checked = false;
+        }
+
+        private void CheckBox2_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox2.Checked)
+                checkBox3.Checked = false;
+        }
+
+        private void CheckBox3_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox3.Checked)
+                checkBox2.Checked = false;
+        }
+    }
+}
